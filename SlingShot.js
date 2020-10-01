7c05deda0d3bd9219d0d4c9bb2a2b853d60dc4f7
jinishk2008 <69375516+jinishk2008@users.noreply.github.com>
2020-09-30 22:05:47 -0500

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,26 @@
+class Slingshot {
+    constructor(bodyX,pointB){
+        var options = {
+            bodyA:bodyX,
+            pointB:pointB,
+            stiffness:0.04,
+            length:10
+        }
+        this.sling = Constraint.create(options)
+        this.pointB = pointB
+        World.add(world, this.sling)
+    }
+    display(){
+        
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+
+        }
+    }
+    fly(){
+
+        this.sling.bodyA=null
+    }
+}
